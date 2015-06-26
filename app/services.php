6fd1afda686f75c1086b22264648770af3bcfedf
Leonhard Melzer <leonhard.melzer@gmail.com>
2015-06-26 17:25:11 +0200

    Use new LDAP based authentication
    Setup the appropriate service providers
    Add sample config file, rename to `config.php` and change values
    accordingly
    Add bare text twig-template
    Add login twig-template

--- app/services.php
+++ app/services.php
@@ -1,17 +1,38 @@
 <?php
 // templating
 $app->register(new Silex\Provider\TwigServiceProvider(), [
-    'twig.path' => __DIR__ . '/../views',
+    'twig.path' => __DIR__ . '/../views'
 ]);
 
 // logging
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-    'monolog.logfile' => __DIR__ . '/../logs/development.log',
-));
+$app->register(new Silex\Provider\MonologServiceProvider(), [
+    'monolog.logfile' => __DIR__ . '/../logs/development.log'
+]);
+
+// ldap
+$app->register(new SOG\Dashboard\ZendLdapServiceProvider(), [
+    'ldap.options' => $config['ldap.options']
+]);
+
+$app->register(new Silex\Provider\SessionServiceProvider());
+
+// used for handy `path()` calls inside Twig templates
+$app->register(new Silex\Provider\UrlGeneratorServiceProvider());
 
 // security / login
-$app->register(new Silex\Provider\SecurityServiceProvider());
-$app->register(new Silex\Provider\RememberMeServiceProvider());
+$app->register(new SOG\Dashboard\Authentication\LdapAuthenticationServiceProvider());
+$app->register(new Silex\Provider\SecurityServiceProvider(), [
+    'security.firewalls' => [
+        'members' => [
+            'pattern' => '^/members/',
+            'users' => function () use ($app) {
+                return $app['security.ldap.user_provider']($app['ldap']);
+            },
+            'form' => ['login_path' => '/login', 'check_path' => '/members/login_check'],
+            'logout' => ['logout_path' => '/members/logout']
+        ]
+    ]
+]);
 
-// TODO: setup
-$app['security.firewalls'] = array();
+// TODO: setup? checkbox?
+$app->register(new Silex\Provider\RememberMeServiceProvider());
--- public/index.php
+++ public/index.php
@@ -1,4 +1,6 @@
 <?php
+use Symfony\Component\HttpFoundation\Request;
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $app = new Silex\Application();
@@ -6,13 +8,48 @@ $app = new Silex\Application();
 // should be disabled on production
 $app['debug'] = true;
 
+require_once __DIR__ . '/../app/config.php';
 require_once __DIR__ . '/../app/services.php';
 
 // now, let's set up the routes
+// TODO: organize in controllers?
 
+// (unprotected) index route
 $app->get('/', function () use ($app) {
     return $app['twig']->render('home.twig', [
         'message' => 'Hello World'
+    ]);
+});
+
+// some LDAP test calls
+$app->get('/ldaptests', function () use ($app) {
+    $content = print_r($app['ldap']->getEntry('uid=leonhard.melzer,ou=active,ou=people,o=sog-de,dc=sog'), true);
+    $content .= print_r(($app['ldap']->search('objectClass=person', 'dc=sog')->getFirst()), true);
+    try {
+        $content .= print_r($app['ldap']->bind('uid=leonhard.melzer,ou=active,ou=people,o=sog-de,dc=sog', 'somePassword'), true);
+    } catch (\Zend\Ldap\Exception\LdapException $ex) {
+        if ($ex->getCode() == \Zend\Ldap\Exception\LdapException::LDAP_INVALID_CREDENTIALS) {
+            $content .= "Der Login war nicht erfolgreich, bitte überprüfe deinen Benutzernamen und Passwort.";
+        } else {
+            $content .= print_r($ex, true);
+        }
+    }
+    return $app['twig']->render('text.twig', [
+        'content' => $content
+    ]);
+});
+
+// this route should be protected
+$app->get('/members/test', function () use ($app) {
+    return $app['twig']->render('home.twig', [
+        'message' => 'Test, should be protected'
+    ]);
+});
+
+$app->get('/login', function (Request $request) use ($app) {
+    return $app['twig']->render('login.twig', [
+        'error' => $app['security.last_error']($request),
+        'last_username' => $app['session']->get('_security.last_username'),
     ]);
 });
 
