563f556dae89d49b3d2e44b4dbc248059084c230
Leonhard Melzer <leonhard.melzer@gmail.com>
2015-06-27 12:10:40 +0200

    Add LdapAdapter
    Implement first management function on LDAP tree
    Make use of those functions in /ldaptests route
    Note: our currently used privileged user doesn’t have write access,
    thus those operations will fail.

--- app/sog/Dashboard/ZendLdapServiceProvider.php
+++ app/sog/Dashboard/ZendLdapServiceProvider.php
@@ -3,7 +3,6 @@ namespace SOG\Dashboard;
 
 use Silex\Application;
 use Silex\ServiceProviderInterface;
-use Zend\Ldap\Ldap;
 
 /**
  * Setup the LDAP resource using Zend\Ldap and share it with the application.
@@ -18,14 +17,9 @@ class ZendLdapServiceProvider implements ServiceProviderInterface
      */
     public function register(Application $app)
     {
-        // provide an alias, independent of the implementation
-        $app['ldap'] = function () use ($app) {
-            return $app['zendldap'];
-        };
-
-        // setup the actual LDAP resource
-        $app['zendldap'] = $app->share(function ($app) {
-            return new Ldap($app['ldap.options']);
+        // setup the ldap adapter which extends the LDAP abstraction layer
+        $app['ldap'] = $app->share(function ($app) {
+            return new LdapAdapter($app['ldap.options']);
         });
     }
 
